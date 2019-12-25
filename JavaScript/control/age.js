e381e00f1bea6796b3e383569c21f6f1532cb13c
bishalraktim <raktimbishal@gmail.com>
2019-12-25 17:24:28 +1100

    Removed unwanted directory

--- JavaScript/control/age.js
+++
@@ -1,3 +0,0 @@
-var age = prompt("Enter your age");
-var alive = age*356 + " " + "days";
-alert(age + " " + "years is equivalent to " + alive );
--- JavaScript/function/string.js
+++
@@ -1,7 +0,0 @@
-function kebabToSanke(key) {
-  var str = key.replace(/-/g , "_");
-  return str;
-}
-
-enterString = prompt("Enter the string!");
-alert(kebabToSanke(enterString));
